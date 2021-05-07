e6d9fb74bd4ec00e519b3aa2bc1a59e9ba89e208
Himanshu-180875 <himanshupahuja55@gmail.com>
2021-05-08 00:21:50 +0530

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
 import "./App.css";
-import Banner from "./components/Banner";
 import Nav from "./components/Nav";
-import Services from "./components/Services";
-import About from "./components/About";
-import Prices from "./components/Prices";
-import {BrowserRouter,Route} from 'react-router-dom';
-import Contact from "./components/Contact";
 
 function App() {
   return (
-   
+  
     <div>
-      <Banner />
-      
-      <Nav />
-      <Services />
-      <About />
-     
-      <Contact />
+    <Nav />    
     </div>
   
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
-
+import Banner from "./Banner";
 import Services from "./Services";
 import About from "./About";
-// import { Link } from 'react-router-dom';
+import Contact from "./Contact";
+
 
 import { FaAlignJustify } from "react-icons/fa";
 class Nav extends Component {
@@ -13,32 +14,64 @@ class Nav extends Component {
     this.state ={
       value: true,
     }
+    this.scrollToTop=this.scrollToTop.bind(this);
+    this.scrollToServices=this.scrollToServices.bind(this);
+    this.scrollToAbout=this.scrollToAbout.bind(this);
+    this.scrollToContact=this.scrollToContact.bind(this);
+   
   }
+  scrollToTop=()=>{
+    scroll.scrollToTop();
+
+  }
+  scrollToServices=()=>{scroller.scrollTo('services', {
+    duration: 1000,
+    
+    smooth: true,
+  })}
+  scrollToAbout = () => {scroller.scrollTo('about', {
+    duration: 1000,
+    
+    smooth: true,
+  })
+
+  }
+  scrollToContact = () => {scroller.scrollTo('contact', {
+    duration: 1000,
+    
+    smooth: true,
+  })
+
+  }
+  
 
   render(){
   
   return (
-    <nav className="navbar">
+    <div>
+    <nav className="navbar" >
       <div className="container">
         <div className="navbar__container">
           <ul className="navbar__left">
-            <div className="navbar__left-logo">
+            <div className="navbar__left-logo" onClick={this.scrollToTop}  style={{cursor:'pointer'}} >
               <img src="https://raw.githubusercontent.com/Himanshu-180875/Himanshu-website/main/public/images/logo1.png" alt="logo" />
             </div>
           </ul>
           {this.state.value ? (
             <ul className="navbar__right">
-              <li>
-                <a href="">Home</a>
+              <li onClick={this.scrollToTop} style={{cursor:'pointer'}}  >
+              duration={500}
+                <a>Home</a>
               </li>
-              <li>
-                <a href="">Projects</a>
+              <li onClick={this.scrollToServices} style={{cursor:'pointer'}}>
+              
+                <a >Projects</a>
               </li>
-              <li>
-                <a href="">About</a>
+              <li onClick={this.scrollToAbout} style={{cursor:'pointer'}}>
+                <a >About</a>
               </li>
-              <li>
-                <a href="">Contact</a>
+              <li onClick={this.scrollToContact} style={{cursor:'pointer'}}>
+                <a>Contact</a>
               </li>
             </ul>
           ) : (
@@ -50,6 +83,11 @@ class Nav extends Component {
         <FaAlignJustify />
       </div>
     </nav>
+    <Banner />
+    <Services name="services" />
+    <About name="about" />
+    <Contact name="contact" />
+    </div>
   );
           }
 };
