d9609cfe546603affdd2fc842e4a8a2a3b3ea3b3
Himanshu-180875 <himanshupahuja55@gmail.com>
2021-05-07 20:49:22 +0530

    Updates

---
+++ precache-manifest.34037c6dcc77fede0e19ad56ad2bb9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c23a36f459670df64e7e869d749cd6c",
+    "url": "/Himanshu-website/index.html"
+  },
+  {
+    "revision": "cbd1bbdca48914b05cd0",
+    "url": "/Himanshu-website/static/css/main.019c3595.chunk.css"
+  },
+  {
+    "revision": "d0f599c54ea055158f6d",
+    "url": "/Himanshu-website/static/js/2.4cb0cfcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Himanshu-website/static/js/2.4cb0cfcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd1bbdca48914b05cd0",
+    "url": "/Himanshu-website/static/js/main.63a47bd0.chunk.js"
+  },
+  {
+    "revision": "6da10774c765adc70e33",
+    "url": "/Himanshu-website/static/js/runtime-main.acc19dbb.js"
+  }
+]);
