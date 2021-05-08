2e64a8a67e5d5946a6b08fe4a95e6bde81a18e61
Himanshu-180875 <himanshupahuja55@gmail.com>
2021-05-08 17:33:52 +0530

    Updates

---
+++ precache-manifest.880010f0d3201d8dd63f905b06bdc5da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71efae078338f5fec23594ff02ec706a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50b4438f66b75134e317",
+    "url": "/static/css/main.c812f3e5.chunk.css"
+  },
+  {
+    "revision": "45dd1a2e36b7591cfe0d",
+    "url": "/static/js/2.723e2cc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.723e2cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b4438f66b75134e317",
+    "url": "/static/js/main.4c5f89a1.chunk.js"
+  },
+  {
+    "revision": "1422f143402fd369506c",
+    "url": "/static/js/runtime-main.c4c9f97b.js"
+  }
+]);
